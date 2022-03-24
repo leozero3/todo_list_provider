04805355a28c38dfcbb65daf82f32be746c5141c
leo <realleo2009@hotmail.com>
2022-03-23 22:44:56 -0300

    todo

--- lib/app/modules/home/home_controller.dart
+++ lib/app/modules/home/home_controller.dart
@@ -17,7 +17,7 @@ class HomeController extends DefaultChangeNotifier {
   List<TaskModel> allTasks = [];
   List<TaskModel> filteredTasks = [];
   DateTime? initialDateOfWeek;
-  DateTime? selectedDate;
+  DateTime? selectedDay;
 
   HomeController({required TasksServices tasksServices})
       : _tasksServices = tasksServices;
@@ -73,8 +73,12 @@ class HomeController extends DefaultChangeNotifier {
     allTasks = tasks;
 
     if (filter == TaskFilterEnum.week) {
-      if (initialDateOfWeek != null) {
+      if (selectedDay != null) {
+        filterByDay(selectedDay!);
+      } else if (initialDateOfWeek != null) {
         filterByDay(initialDateOfWeek!);
+      } else {
+        selectedDay = null;
       }
     }
     hideLoading();
@@ -82,7 +86,7 @@ class HomeController extends DefaultChangeNotifier {
   }
 
   void filterByDay(DateTime date) {
-    selectedDate = date;
+    selectedDay = date;
 
     filteredTasks = allTasks.where((task) {
       return task.dateTime == date;
